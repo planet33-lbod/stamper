4bf5ec9b07757b18aaf2aa6411287ec9c3e2bbb6
planet33-lbod <160257531+planet33-lbod@users.noreply.github.com>
2024-02-19 15:05:06 +0100

    removed other stamps, removed X,Y input

--- ministamp.py
+++ ministamp.py
@@ -9,24 +9,24 @@ def stamp_pdf():
     # Retrieve PDF file from the request
     pdf_file = request.data  # Get binary data directly
     
+    # Apply stamp logic with hardcoded coordinates and image inside the function
+    stamped_pdf_bytes = add_image_to_pdf(io.BytesIO(pdf_file))
+    
+    # Return the stamped PDF
+    stamped_pdf_bytes.seek(0)  # Rewind to the beginning of the file before sending
+    return send_file(stamped_pdf_bytes, attachment_filename='stamped.pdf', as_attachment=True)
+
+def add_image_to_pdf(input_pdf_bytes):
     # Hardcode the stamp image path
     image_path = 'gebucht.png'
     
     # Hardcode the coordinates for the stamp
     x_coord, y_coord = (200, 100)
     
-    # Apply stamp logic with hardcoded coordinates and image
-    stamped_pdf_bytes = add_image_to_pdf(pdf_bytes=io.BytesIO(pdf_file), image_path=image_path, x_coord=x_coord, y_coord=y_coord)
-    
-    # Return the stamped PDF
-    stamped_pdf_bytes.seek(0)  # Rewind to the beginning of the file before sending
-    return send_file(stamped_pdf_bytes, attachment_filename='stamped.pdf', as_attachment=True)
-
-def add_image_to_pdf(input_pdf_bytes, image_path, x_coord, y_coord):
     doc = fitz.open(stream=input_pdf_bytes, filetype='pdf')
     page = doc[0]  # Assuming stamping the first page
     
-    # Define the rectangle where the image will be placed
+    # Define the rectangle where the image will be placed (hardcoded size)
     img_rect = fitz.Rect(x_coord, y_coord, x_coord + 100, y_coord + 100)
     
     # Insert the image
